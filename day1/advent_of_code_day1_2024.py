ab7c7038f3985fd07f8800e5c2cc1828268a0cdb
colinwehrhan <colin.wehrhan@gmail.com>
2024-12-02 20:08:58 -0800

    finished day1 part2

--- day1/advent_of_code_day1_2024.py
+++ day1/advent_of_code_day1_2024.py
@@ -5,17 +5,28 @@ file_path = "C:\\Users\\ColinWehrhan\\Documents\\adventOfCode2024\\day1\\adventO
 
 df = pd.read_csv(file_path, delim_whitespace=True, header=None)
 
-df.columns = ['Column1', 'Column2']
+df.columns = ['left_list', 'right_list']
 
 
 print(df.iloc[0])
 
-df['Column1'] = df['Column1'].sort_values(ascending=False).values
-df['Column2'] = df['Column2'].sort_values(ascending=False).values
+df['left_list'] = df['left_list'].sort_values(ascending=False).values
+df['right_list'] = df['right_list'].sort_values(ascending=False).values
 
 
-df['Difference'] = abs(df['Column1'] - df['Column2'])
+df['Difference'] = abs(df['left_list'] - df['right_list'])
 
 sum_difference = df['Difference'].sum()
 
 print(sum_difference)
+
+
+#part 2
+counts = []
+
+for value in df['left_list']:
+    count = (df['right_list']== value).sum()
+    counts.append(count*value)
+
+total = sum(counts)
+print(total)
