e8669075de4d60823099d3c8c9cd741f8e207cee
colinwehrhan <colin.wehrhan@gmail.com>
2024-12-08 19:13:49 -0800

    redid parts of the code, still not right

--- day2/day2.py
+++ day2/day2.py
@@ -1,12 +1,6 @@
-#import data into data frame
-
 import pandas as pd
 
 file_path = "C:\\Users\\ColinWehrhan\\Documents\\adventOfCode2024\\day2\\input.txt"
-
-import pandas as pd
-
-#df = pd.read_csv(file_path, sep='\s+', header=None, dtype=object)
 
 with open(file_path, 'r') as file:
     data = [list(map(int, line.split())) for line in file]
@@ -15,19 +9,14 @@ df = pd.DataFrame(data)
 
 df_t = df.T
 
+print("Original Transposed DataFrame:")
+print(df_t)
+
+# Loop through columns and drop any that are increasing by more than 3 and check if they are increasing/decreasing
 for column in df_t.columns:
-    if not (
-        ((df_t[column].diff().iloc[1:] >= 0) | (df_t[column].diff().iloc[1:] <= 0))  
-        & (df_t[column].diff().iloc[1:].abs() <= 2)
-    ).all():
+    diffs = df_t[column].diff().iloc[1:]
+    
+    if not (diffs.abs() <= 3).all() or not (diffs.is_monotonic_increasing or diffs.is_monotonic_decreasing):
         df_t = df_t.drop(column, axis=1)
 
-count = df.shape[1]
-
-print(count)
-
-
-
-
-
-
+print(df_t)
